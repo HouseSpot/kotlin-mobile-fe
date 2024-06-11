18f6774d09412426894b53d54edc681782b493b1
Fitria Dwi Cahya <fitria.dwi12@ui.ac.id>
2024-06-11 13:46:02 +0700

    Add FE for homepage,account,pesanan,layanan, and prediksi harga feature

---
+++ app/src/main/java/com/entsh118/housespot/layananjasa/FormPesananActivity.kt
@@ -0,0 +1,21 @@
+package com.entsh118.housespot.layananjasa
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.entsh118.housespot.R
+
+class FormPesananActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_form_pesanan)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
