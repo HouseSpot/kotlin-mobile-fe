e81da096481e8417a739c76eb15891776c669935
achievagemilang <achievafuturagemilang@gmail.com>
2024-06-15 15:09:53 +0700

    feat: defining order model

---
+++ app/src/main/java/com/entsh118/housespot/data/api/model/Order.kt
@@ -0,0 +1,16 @@
+package com.entsh118.housespot.data.api.model
+
+data class Order(
+    val id: String,
+    val id_pemesan: String,
+    val id_vendor: String,
+    val projectDescription: String,
+    val serviceType: String,
+    val propertyType: String,
+    val materialProvider: String,
+    val startDate: String,
+    val endDate: String,
+    val budget: Long,
+    val status: String
+)
+
