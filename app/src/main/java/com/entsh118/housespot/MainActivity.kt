9d7bb4b007558215f54991cc95041d0331aa66cb
achievagemilang <achievafuturagemilang@gmail.com>
2024-06-15 12:41:07 +0700

    feat: initial auth logic

--- app/src/main/java/com/entsh118/housespot/MainActivity.kt
+++ app/src/main/java/com/entsh118/housespot/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.entsh118.housespot
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.entsh118.housespot.ui.auth.OnboardingActivity
+import com.entsh118.housespot.ui.homepage.HomePageActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val isLoggedIn = checkUserLoggedIn()
+
+            if (isLoggedIn) {
+                startActivity(Intent(this, HomePageActivity::class.java))
+            } else {
+                startActivity(Intent(this, OnboardingActivity::class.java))
+            }
+            finish()
+        }, 3000)
+    }
+
+    private fun checkUserLoggedIn(): Boolean {
+        // TODO: implement logic to check if user is logged in
+        return false
     }
 }
