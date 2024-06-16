6d9294b4d1adab7e9b11a1027bef0f486233d9a4
Fitria Dwi Cahya <fitria.dwi12@ui.ac.id>
2024-06-16 21:20:32 +0700

    before pull new from dev

---
+++ app/src/main/java/com/entsh118/housespot/ui/layananjasa/ListVendorActivity.kt
@@ -0,0 +1,21 @@
+package com.entsh118.housespot.ui.layananjasa
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.entsh118.housespot.R
+
+class ListVendorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_list_vendor)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
